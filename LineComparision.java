b5b643042f332214270bf1edd1490df3a0f4b56e
msnkittu859 <Vamshikalyan97>
2022-06-01 20:19:30 +0530

    WelCome

---
+++ LineComparision.java
@@ -0,0 +1,9 @@
+package kittu;
+
+public class LineComparision {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to line comparision computation on master branch");
+
+}
+}
